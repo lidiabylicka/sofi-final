22e3acf450fab7446801f3ee8338a30073b8a679
Lidia <lidiabylicka@gmail.com>
2023-12-24 12:07:58 +0000

    msg

--- sofi-qr/src/App.jsx
+++ sofi-qr/src/App.jsx
@@ -3,13 +3,14 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1>HAPPY BDAY SIS!</h1>
+      <h1 className='mt-10'>HAPPY BDAY SIS!</h1>
       <div className='card'>
         <p>
-          <span className='bold'>Sofi, I hope you have a wonderful day</span>
+          <h3 className='bold'>Sofi, I hope you have a wonderful day</h3>
         </p>
         <div className='mx-10 my-10 px-10 py-10 border border-black rounded bg-aqua'>
           <h1>This is your voucher for ¥ 15,000 for your May trip ♡ </h1>
+          <h2>Let me know when you're ready to claim it ^^</h2>
         </div>
       </div>
     </>
