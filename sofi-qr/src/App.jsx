795a0f66f3971d4743bb534b30d169bfb5848393
Lidia <lidiabylicka@gmail.com>
2023-12-24 11:57:50 +0000

    commimt

--- sofi-qr/src/App.jsx
+++ sofi-qr/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <p>
           <span className='bold'>Sofi, I hope you have a wonderful day</span>
         </p>
-        <div className='mx-10 my-10 px-10 py-10 border border-black rounded'>
+        <div className='mx-10 my-10 px-10 py-10 border border-black rounded bg-aqua'>
           <h1>This is your voucher for ¥ 15,000 for your May trip ♡ </h1>
         </div>
       </div>
