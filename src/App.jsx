dac9d77e53095251f87a448a5bf1fa76386c1c00
Lidia <lidiabylicka@gmail.com>
2023-12-26 09:01:31 +0000

    ap

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ function App() {
   return (
     <>
       <h1 className='mt-10'>HAPPY BDAY SIS!</h1>
-      <div className='card'>
+      <div>
         <p>
           <h3 className='bold'>Sofi, I hope you have a wonderful day</h3>
         </p>
-        <div className='mx-10 my-10 px-10 py-10 border border-black rounded bg-aqua'>
-          <h1>This is your voucher for ¥ 15,000 for your May trip ♡ </h1>
+        <div className='card mx-10 my-10 px-10 py-10 bg-blue-500'>
+          <h1>This is your voucher for ¥15,000 for your May trip ♡ </h1>
           <h2>Let me know when you're ready to claim it ^^</h2>
         </div>
       </div>
