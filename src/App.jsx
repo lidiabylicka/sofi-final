c3d0bec3ad5f3ec2e1b129bb75979c2f7101e370
Lidia <lidiabylicka@gmail.com>
2023-12-27 13:43:25 +0000

    Add existing project files to Git

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1 className='mt-10'>HAPPY BDAY SIS! ✨ 🥳 🎂</h1>
+      <h1 className='mt-10'>HAPPY BDAY SIS! </h1>
       <div>
-        <p>
-          <h3 className='bold'>Sofi, I hope you have a wonderful day</h3>
-        </p>
+        <h1>✨ 🥳 🎂</h1>
         <div className='card mx-10 my-10 px-10 py-10 bg-blue-500'>
-          <h1>This is your voucher for ¥15,000 for your May trip ♡ </h1>
-          <h2>Let me know when you're ready to claim it ^^</h2>
+          <h1>VOUCHER FOR</h1>
+          <h1>¥15,000</h1>
+          <h1>for your Japan trip ♡ </h1>
+          <h2>Let me know when you`re ready to claim ^^</h2>
         </div>
       </div>
+      <a href='mailto:lidiabylicka@gmail.com?subject=Claim%20JapanVoucher%20Now'>
+        CLAIM NOW
+      </a>
     </>
   );
 }
